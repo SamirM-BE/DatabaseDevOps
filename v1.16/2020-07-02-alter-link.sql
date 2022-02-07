b902bfb3af98b66df9482181fe932922dd2a022a
Samir M <marini.samir.info@gmail.com>
2022-02-07 08:13:08 +0000

    reset repo

--- v1.16/2020-07-02-alter-link.sql
+++
@@ -1,9 +0,0 @@
---liquibase formatted sql
---changeset samir:3 context:PROD labels:v1.16
---comment added column last_update
-
-ALTER TABLE link ADD last_update DATE;
-
-
---rollback ALTER TABLE link DROP COLUMN last_update;
-
